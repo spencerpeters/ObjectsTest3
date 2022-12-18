7ae3e50f742e3d9b0bd649fec17aabf642b9de9c
Spencer Peters <sp2473@cornell.edu>
2022-12-17 21:32:31 -0500

    Initial Commit

---
+++ ObjectsTest3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ObjectsTest3
+//
+//  Created by Spencer Peters on 12/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
